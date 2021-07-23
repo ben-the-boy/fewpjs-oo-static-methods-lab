8f66a930d5671fe01a4e433c3b5aa0ee35690a27
Ben Hayes <b.t.hayes@comcast.net>
2021-07-22 19:07:03 -0500

    commit tree 85ec4bd9e7998abd39421eb25ea32a3fe55b255e

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(text) {
+    return text[0].toUpperCase() + text.slice(1);
+  }
+
+  static sanitize(text) {
+    return text.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(text) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = text.split(" ")
+    for (let n = 0; n < arrayOfWords.length; n++) {
+      if (n == 0) {
+        result.push(this.capitalize(arrayOfWords[n]))
+      } else {
+        if (exceptions.includes(arrayOfWords[n])) {
+          result.push(arrayOfWords[n])
+        } else {
+          result.push(this.capitalize(arrayOfWords[n]))
+        }
+      }
+
+    }
+    return result.join(" ");
+  }
 }
